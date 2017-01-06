d1c83f5db136416e26064314dd494d4bed7d5073
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-06 05:13:53 -0800

    05: create a table view (wip--design custom table cells)

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Suzannah Kirk-Daligcon on 1/6/17.
+//  Copyright © 2017 SK. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
